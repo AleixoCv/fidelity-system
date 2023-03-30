e73b04cbfdcbedda53c8c910b762a5b07bbfa4f0
David Ian <78829640+Davidian19@users.noreply.github.com>
2023-03-30 10:20:51 -0300

    Add files via upload

---
+++ src/br/gov/cesarschool/fidelidade/cliente/negocio/Cliente.java
@@ -0,0 +1,10 @@
+package br.gov.cesarschool.poo.fidelidade.cliente.negocio;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+
+import br.gov.cesarschool.poo.fidelidade.geral.negocio.Endereco;
+import br.gov.cesarschool.poo.fidelidade.geral.negocio.Sexo;
+
+public class Cliente
