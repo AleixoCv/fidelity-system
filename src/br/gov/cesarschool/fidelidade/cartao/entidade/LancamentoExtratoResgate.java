cd8e1594253693759c620947d47f908a0442f04a
David ian <contatodavidian@gmail.com>
2023-04-01 10:20:56 -0300

    ajustes dos diretórios de 'negocio' para 'entidade'

--- src/br/gov/cesarschool/fidelidade/cartao/entidade/LancamentoExtratoResgate.java
+++ src/br/gov/cesarschool/fidelidade/cartao/entidade/LancamentoExtratoResgate.java
@@ -1,9 +1,3 @@
-// Classe: LancamentoExtratoResgate, deve herdar de LancamentoExtrato
-// Pacote: br.gov.cesarschool.poo.fidelidade.cartao.negocio
-// Atributos: os herdados e...
-// • tipoResgate – br.gov.cesarschool.poo.fidelidade.cartao.negocio.TipoResgate
-// Construtor: deve inicializar todos os atributos.
-// Métodos: get público para tipoResgate.
 public class LancamentoExtratoResgate extends LancamentoExtrato{
     private TipoResgate tipoResgate;
     
