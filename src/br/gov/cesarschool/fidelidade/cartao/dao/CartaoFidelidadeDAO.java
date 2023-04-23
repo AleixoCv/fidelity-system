49490f5b1ed9972ddf514fa4181fd10b28b287b3
Aleixo <dac2@cesar.school>
2023-04-23 10:52:37 -0300

    Included DAO

--- src/br/gov/cesarschool/fidelidade/cartao/dao/CartaoFidelidadeDAO.java
+++ src/br/gov/cesarschool/fidelidade/cartao/dao/CartaoFidelidadeDAO.java
@@ -1,14 +1,89 @@
-public class CartaoFidelidadeDAO extends CartaoFidelidade{
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-    public boolean incluir(CartaoFidelidade cartao){
-        return false;
-    }
-    
-    public boolean alterar(CartaoFidelidade cartao){
-        return false;
-    }
+public class CartaoFidelidadeDAO {
+	private static final String FILE_SEP = System.getProperty("file.separator");
+	private static final String DIR_BASE = "." + FILE_SEP + "fidelidade" + FILE_SEP 
+			+ "cartao" + FILE_SEP; 
+	private static final String EXT = ".dat";
+	public CartaoFidelidadeDAO() {
+		File diretorio = new File(DIR_BASE);
+		if (!diretorio.exists()) {
+			diretorio.mkdir();			
+		}
+	}
+	private File getArquivo(long numero) {
+		String nomeArq = DIR_BASE + numero + EXT;
+		return new File(nomeArq);		
+	}
 
-    public CartaoFidelidade buscar(long numero){
-        return null;
-    }
+	private void incluirAux(CartaoFidelidade cartaoFidelidade, File arq) {
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+		try {
+			fos = new FileOutputStream(arq);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(cartaoFidelidade);
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao incluir CartaoFidelidade");
+		} finally {
+			try {
+				oos.close();
+			} catch (Exception e) {}
+			try {
+				fos.close();
+			} catch (Exception e) {}			
+		} 		
+	}
+	public boolean incluir(CartaoFidelidade cartaoFidelidade) {
+		File arq = getArquivo(cartaoFidelidade.getNumero());
+		if (arq.exists()) {
+			return false; 
+		}
+		incluirAux(cartaoFidelidade, arq);
+		return true; 
+	}
+	public boolean alterar(CartaoFidelidade cartaoFidelidade) {
+		File arq = getArquivo(cartaoFidelidade.getNumero());
+		if (!arq.exists()) {
+			return false; 
+		}		
+		if (!arq.delete()) {
+			return false;
+		}
+		incluirAux(cartaoFidelidade, arq);
+		return true;
+	}
+	public boolean excluir(long numero) {
+		File arq = getArquivo(numero);
+		if (!arq.exists()) {
+			return false; 
+		}				
+		return arq.delete();
+	}
+	public CartaoFidelidade buscar(long numero) {
+		File arq = getArquivo(numero);
+		if (!arq.exists()) {
+			return null; 
+		}				
+		FileInputStream fis = null;
+		ObjectInputStream ois = null;
+		try {
+			fis = new FileInputStream(arq);
+			ois = new ObjectInputStream(fis);
+			return (CartaoFidelidade)ois.readObject(); 
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao ler cartao fidelidade");
+		} finally {
+			try {
+				ois.close(); 
+			} catch (Exception e) {}
+			try {
+				fis.close(); 
+			} catch (Exception e) {}			
+		}
+	}
 }
--- src/br/gov/cesarschool/fidelidade/cartao/dao/LancamentoExtratoDAO.java
+++ src/br/gov/cesarschool/fidelidade/cartao/dao/LancamentoExtratoDAO.java
@@ -1,10 +1,51 @@
-public class LancamentoExtratoDAO {
-     
-    public boolean incluir(LancamentoExtratoPontuacao pontuacao){
-        return false;
-    }
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public booelan incluir(LancamentoExtratoResgate resgate){
-        return false;
-    }
+public class LancamentoExtratoDAO {
+	private static final String FILE_SEP = System.getProperty("file.separator");
+	private static final String DIR_BASE = "." + FILE_SEP + "fidelidade" + FILE_SEP 
+			+ "lancamento" + FILE_SEP; 
+	private static final String EXT = ".dat";
+	public LancamentoExtratoDAO() {
+		File diretorio = new File(DIR_BASE);
+		if (!diretorio.exists()) {
+			diretorio.mkdir();
+		}
+	}
+	private File getArquivo(long numero, String tipo) {
+		String timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+		String chaveLancamento = tipo + numero + "_" + timestamp; 
+		String nomeArq = DIR_BASE + chaveLancamento + EXT;
+		return new File(nomeArq);		
+	}
+	private void incluirAux(LancamentoExtrato lancamentoExtrato, String tipo) {
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+		 File arq = getArquivo(lancamentoExtrato.getNumeroCartao(), tipo);
+		try {
+			fos = new FileOutputStream(arq);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(lancamentoExtrato);
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao incluir lancamento");
+		} finally {
+			try {
+				oos.close();
+			} catch (Exception e) {}
+			try {
+				fos.close();
+			} catch (Exception e) {}			
+		} 		
+	}	
+	public boolean incluir(LancamentoExtratoPontuacao pontuacao) {
+		incluirAux(pontuacao, "C");
+		return true;
+	}
+	public boolean incluir(LancamentoExtratoResgate resgate) {
+		incluirAux(resgate, "D");
+		return true;
+	}	
 }
